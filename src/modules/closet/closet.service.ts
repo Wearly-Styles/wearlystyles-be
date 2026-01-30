bb1a220f2a6a1b933134f746946384d57a96dea8
Bang-2108 <zoanthibang@gmail.com>
2026-01-30 15:14:31 +0700

    feat:(WS-24) update service logic

--- src/modules/closet/closet.service.ts
+++ src/modules/closet/closet.service.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 export const getWardrobeData = async (userId: number) => {
-  let items = await prisma.clothingItem.findMany({
-    where: { userId: userId }
+  const items = await prisma.clothingItem.findMany({
+    where: {
+      OR: [
+        { isDefault: true }, 
+        { userId: userId }   
+      ]
+    },
+    orderBy: {
+      isDefault: 'desc' 
+    }
   });
-  if (items.length === 0) {
-    items = await prisma.clothingItem.findMany({
-      where: { isDefault: true }
-    });
-  }
   return items;
 };
